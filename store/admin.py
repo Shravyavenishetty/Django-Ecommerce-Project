88fd3e96d0145bdf1fa5b17f869df4f7dd419ac0
Venishetty Shravya <shravyavenishetty03@gmail.com>
2025-07-23 16:07:04 +0530

    feat:created admin classes for product and variation

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -8,4 +8,11 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ('is_available', 'category')
     search_fields = ('product_name', 'description')
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value')
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)  # Assuming Variation is defined in the same file or imported correctly
+
