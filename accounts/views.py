ac330eb519915f9894d1d3d711ece1e17d70e486
Venishetty Shravya <shravyavenishetty03@gmail.com>
2025-07-26 12:46:50 +0530

    forgotPassword function

--- accounts/views.py
+++ accounts/views.py
@@ -102,3 +102,6 @@ def activate(request,uidb64,token):
 @login_required(login_url='login')
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
+
+def forgotPassword(request):
+    return render(request,'accounts/forgotPassword.html')
